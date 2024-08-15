f8c4fb4ffb3d34f999e67e72f749ff6333a9f8a4
AdyaS2010 <adya.sastry@gmail.com>
2024-08-15 13:47:50 -0500

    Update trie.c

--- trie.c
+++ trie.c
@@ -47,4 +47,43 @@ int main(int argc, char *argv[])
 
     // Allocate root of trie
     root = malloc(sizeof(node));
-}
+
+    if (root == NULL)
+    {
+        return 1;
+    }
+
+    root->is_word = false;
+    for (int i = 0; i < SIZE_OF_ALPHABET; i++)
+    {
+        root->children[i] = NULL;
+    }
+
+    // Add words to the trie
+    while (fscanf(infile, "%s", name) == 1)
+    {
+        node *cursor = root;
+
+        for (int i = 0, n = strlen(name); i < n; i++)
+        {
+            int index = tolower(name[i]) - 'a';
+            if (cursor->children[index] == NULL)
+            {
+
+                // Make node
+                node *new = malloc(sizeof(node));
+                new->is_word = false;
+                for (int j = 0; j < SIZE_OF_ALPHABET; j++)
+                {
+                    new->children[j] = NULL;
+                }
+                cursor->children[index] = new;
+            }
+
+            // Go to node which (we) may have just (been) made
+            cursor = cursor->children[index];
+        }
+
+        // if we are at the end of the word, mark it as being a word
+        cursor->is_word = true;
+    }
